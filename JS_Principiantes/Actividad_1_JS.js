e46c6060f86e3ab9bc019622f7bb4526bc468ee8
Alex8aa <124051154@upq.edu.mx>
2025-09-10 15:20:44 -0600

    Tipos de datos
    Se escriben los tipos de datos con los que podemos trabajar

--- JS_Principiantes/Actividad_1_JS.js
+++ JS_Principiantes/Actividad_1_JS.js
@@ -2,3 +2,43 @@ console.log("Hello World");
 document.write("Hello World");
 console.error("Mi error customizado");
 console.error("La aplicación ha fallado");
+//tipo de datos
+"Hello World" //string
+"Hello World" //string
+
+1000000 //number
+-2.3
+
+
+//boolean
+true 
+false
+
+//array
+["joe", "ryan", "martha"]
+[1,2,3]
+[true, false, true, false]
+
+
+
+//object 
+{
+    "username": "ryan",
+    "score": 70.4,
+    "hours": 14,
+    "proffesional": true
+
+    "username" : "joe",
+     "score": 0.4,
+    "hours": 1,
+    "proffesional": false
+    
+}
+
+console.log("esto es una cadena")
+console.log("14.3")
+console.log("true")
+console.log(["1,2,3"])
+
+//un objeto empieza con llaves 
+console.log({"username" : "ryan", "score" : 70.4});
