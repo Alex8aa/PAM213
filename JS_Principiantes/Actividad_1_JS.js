8bbf97e0bc2b58d136b4c9ad4772105dd52b4962
Alex8aa <124051154@upq.edu.mx>
2025-09-10 18:07:33 -0600

    Commit 4

--- JS_Principiantes/Actividad_1_JS.js
+++ JS_Principiantes/Actividad_1_JS.js
@@ -1,24 +1,20 @@
-//bucles
-//let count = 0;
-
 //while (count<50){
-//console.log(count);
-//count.log(count);
+//console.log(count)
 //count++;
 //}
 
 //let names = ["ryan", "joe", "john"];
 
+//for(let i =names.length)-1; 1>=0; i--) {
+//console.log(names[i])}
 
-//for(let i = names.length -1; i>=0; i--){
- //console.log(names[i]);
- //}
-
-
- //funcionesfunction add(n1, n2){
- console.log(n1,n2)
-//}
+//funcionesfunction add (n1,n2) {
+// console.log(n1+n2);}
 
 add(3,2);
-add(100, 300);
+add(100,300);
 
+
+add(3,2);
+add(100,300);
+add(312131213121321, 1323112331321);
