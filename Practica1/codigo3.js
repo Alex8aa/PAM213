0d134fa60232245ecf9a3fce5b0f538b0d1fb52d
Alex8aa <124051154@upq.edu.mx>
2025-09-17 20:26:46 -0600

    Hicimos el código 3 con las peticiones

---
+++ Practica1/codigo3.js
@@ -0,0 +1,8 @@
+const saludoPersonalizado = (nombre, edad) => {
+    return `Hola, me llamo ${nombre} y tengo ${edad} años.`;
+
+};
+
+console.log(saludoPersonalizado("Isay", 37));
+console.log(saludoPersonalizado("Emiliano", 25));
+console.log(saludoPersonalizado("José José", 19));
