4c538ce242d38985a802b0bf3d1b9649d66bd804
Alex8aa <124051154@upq.edu.mx>
2025-09-25 15:01:51 -0600

    Resolvimos el segundo ejercicio

---
+++ Practica3/verificarUsuario.js
@@ -0,0 +1,27 @@
+
+function verificarUsuario(usuario) {
+  return new Promise((resolve, reject) => {
+    if (usuario === "admin") {
+      resolve("Acceso concedido");
+    } else {
+      reject("Acceso denegado");
+    }
+  });
+}
+
+
+verificarUsuario("admin")
+  .then((mensaje) => {
+    console.log(mensaje); 
+  })
+  .catch((error) => {
+    console.log(error); 
+  });
+
+verificarUsuario("Javier Hernández")
+  .then((mensaje) => {
+    console.log(mensaje);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
